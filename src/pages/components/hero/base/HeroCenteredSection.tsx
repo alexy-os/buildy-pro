3239847beb010c48d3c2c158257057059c3b36c7
alexy-os <agorbunoff.ru@gmail.com>
2024-12-17 19:00:34 +0300

    navbar to hero

--- src/pages/components/hero/base/HeroCenteredSection.tsx
+++ src/pages/components/hero/base/HeroCenteredSection.tsx
@@ -1,6 +1,7 @@
 import { Info, Rocket } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { NavbarMenuNoLink } from "../../HTMLNavbar";
 
 type Content = {
   badge: string;
@@ -41,6 +42,8 @@ const content: Content = {
 } as const;
 
 export const HeroCenteredSection = () => (
+  <>
+  <NavbarMenuNoLink />
   <section className="w-full py-16 lg:py-32">
     <div className="container mx-auto px-4 md:px-6 lg:px-8">
       <div className="flex flex-col text-center gap-8 items-center">
@@ -65,4 +68,5 @@ export const HeroCenteredSection = () => (
       </div>
     </div>
   </section>
+  </>
 );
